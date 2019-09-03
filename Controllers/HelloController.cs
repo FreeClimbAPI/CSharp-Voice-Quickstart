1301992528abedb1ae9f52c280276b4135c602db
Geoffrey Miller <gmiller@vailsys.com>
2019-09-03 11:44:04 -0500

    Create Getting Started Tutorial

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using com.persephony.api;
+using com.persephony.percl;
+using com.persephony;
+using com.persephony.webhooks;
+
+namespace GettingStarted.Controllers
+{
+    [Route("voice/")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // POST voice/
+        [HttpPost]
+        public string Post(PersyRequest request)
+        {
+          // Create a PerCl script
+          PerCLScript helloScript = new PerCLScript();
+
+          // Create a Say Command
+          Say sayHello = new Say();
+          sayHello.setText("Hello, Persephony!");
+          sayHello.setLanguage(com.persephony.ELanguage.EnglishUS);
+
+          // Add the command
+          helloScript.Add(sayHello);
+
+          // Respon to Persephony with your script
+          return helloScript.toJson();
+        }
+    }
+}
